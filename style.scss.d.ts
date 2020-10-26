14e836f20d24ad774ad7f4d7ed14d1a704f76705
santi <santitok21@gmail.com>
2020-10-26 09:43:21 +0700

    info.sehat

--- style.scss.d.ts
+++ style.scss.d.ts
@@ -2,15 +2,24 @@ import { StyleProp, TextStyle } from 'react-native'
 
 export const menu: StyleProp<TextStyle>
 export const btn1: StyleProp<TextStyle>
+export const btn2: StyleProp<TextStyle>
+export const btn3: StyleProp<TextStyle>
 export const pengertian: StyleProp<TextStyle>
+export const boxp: StyleProp<TextStyle>
 export const sejarah: StyleProp<TextStyle>
+export const boxs: StyleProp<TextStyle>
 export const penyebab: StyleProp<TextStyle>
+export const boxb: StyleProp<TextStyle>
 export const gejala: StyleProp<TextStyle>
 export const sesak: StyleProp<TextStyle>
 export const bersin: StyleProp<TextStyle>
 export const batuk: StyleProp<TextStyle>
 export const demam: StyleProp<TextStyle>
 export const pencegahan: StyleProp<TextStyle>
+export const kebersihan: StyleProp<TextStyle>
+export const cucitangan: StyleProp<TextStyle>
+export const olahraga: StyleProp<TextStyle>
+export const vitc: StyleProp<TextStyle>
 export const about: StyleProp<TextStyle>
 export const title: StyleProp<TextStyle>
 export const link: StyleProp<TextStyle>
