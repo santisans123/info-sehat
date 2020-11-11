d197efb779a9087dc5440fab0608c3241546978e
santi <santitok21@gmail.com>
2020-11-11 13:37:06 +0700

    infosehat

--- css/style.scss.d.ts
+++ css/style.scss.d.ts
@@ -2,9 +2,9 @@ import { StyleProp, TextStyle } from 'react-native'
 
 export const container: StyleProp<TextStyle>
 export const runningtext: StyleProp<TextStyle>
+export const navbar: StyleProp<TextStyle>
 export const menu: StyleProp<TextStyle>
 export const btn: StyleProp<TextStyle>
-export const menu-toggle: StyleProp<TextStyle>
 export const sidebar: StyleProp<TextStyle>
 export const widged: StyleProp<TextStyle>
 export const gif: StyleProp<TextStyle>
@@ -19,3 +19,5 @@ export const gambar_healthcare: StyleProp<TextStyle>
 export const about: StyleProp<TextStyle>
 export const title: StyleProp<TextStyle>
 export const link: StyleProp<TextStyle>
+export const menu-toggle: StyleProp<TextStyle>
+export const gambarflu: StyleProp<TextStyle>
